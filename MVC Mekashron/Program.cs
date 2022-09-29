470bb6f2c61c5b881c432765a16d59c2bbff71dd
Movsar Bekaev <bekaev.movsar@gmail.com>
2022-09-29 23:10:40 +0300

    improved styling

--- MVC Mekashron/Program.cs
+++ MVC Mekashron/Program.cs
@@ -15,8 +15,6 @@ namespace MVC_Mekashron
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
