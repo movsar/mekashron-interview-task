6d6f445b4806f62f9b7e490f627aa081aa4ede18
Movsar Bekaev <bekaev.movsar@gmail.com>
2022-09-29 21:36:34 +0300

    cleaning

---
+++ MVC Mekashron/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using ICUTechServiceReference;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MVC_Mekashron.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginRequest loginRequest)
+        {
+            var client = new ICUTechServiceReference.ICUTechClient();
+            //var rawResponse = await client.LoginAsync(username, password, "");
+            //ViewBag.ResponseData = JsonConvert.DeserializeObject(rawResponse.@return);
+
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
