b8531e849cd299da329c0c76ef98927026905be7
Movsar Bekaev <bekaev.movsar@gmail.com>
2022-09-29 19:46:30 +0300

    works nicely

---
+++ MVC Mekashron/Controllers/SignInController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace MVC_Mekashron.Controllers
+{
+    public class SignInController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Index(string username, string password)
+        {
+            var client = new ICUTechServiceReference.ICUTechClient();
+            var rawResponse = await client.LoginAsync(username, password, "");
+            JObject responseData = (JObject)JsonConvert.DeserializeObject(rawResponse.@return);
+
+            ViewData["ResponseData"] = responseData;
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
